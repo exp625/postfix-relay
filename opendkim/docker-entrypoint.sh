e47f12a02d159ebac8606eb3a08c38a1f50f94d4
Tim <xmrtimex@gmail.com>
2020-12-02 21:59:04 +0100

    :bug: Trying to fix key problem

--- opendkim/docker-entrypoint.sh
+++ opendkim/docker-entrypoint.sh
@@ -5,19 +5,13 @@ do
 done
 
 
-for domain in $(cat /etc/opendkim/TrustedHosts)
-do
-    if [[ $domain == "localhost" || $domain == "127.0.0.1" || "::1" ]]; then
-        continue
-    else
-        if [ ! -d "/tmp/keys/${domain:2}" ]; then
-            mkdir -p /tmp/keys/${domain:2}
-            cd /tmp/keys/${domain:2}
-            opendkim-genkey -s mail -d ${domain:2}
-           chown opendkim:opendkim mail.private
-        fi
-    fi
-done
+if [ ! -d "/tmp/keys/${DOMAIN}" ]; then
+    mkdir -p /tmp/keys/${DOMAIN}
+    cd /tmp/keys/${DOMAIN}
+    opendkim-genkey -s mail -d ${DOMAIN}
+    chown opendkim:opendkim mail.private
+fi
+
 
 cp -R /tmp/keys /etc/opendkim/keys
 chown -R  opendkim:opendkim /etc/opendkim/keys
