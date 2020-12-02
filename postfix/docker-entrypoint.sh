56119588f51adc6bf6500ad074513b90708165e6
Tim <xmrtimex@gmail.com>
2020-12-02 21:08:11 +0100

    :bug: Trying to fix permission problems

--- postfix/docker-entrypoint.sh
+++ postfix/docker-entrypoint.sh
@@ -3,7 +3,7 @@ for config in /config/*
 do
   envsubst < $config > /etc/postfix/${config##*/}
 done
-
+usermod -a -G opendkim postfix
 postmap /etc/postfix/NetworkTable
 /etc/postfix/post-install meta_directory=/etc/postfix create-missing
 postfix set-permissions
