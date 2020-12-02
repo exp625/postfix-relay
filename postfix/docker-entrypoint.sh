7e6b66f204a6d6074bf6ef2add38064198ba70e4
Tim <xmrtimex@gmail.com>
2020-12-02 20:57:35 +0100

    :bug: Create Postfix DB and log to dev/stdout

--- postfix/docker-entrypoint.sh
+++ postfix/docker-entrypoint.sh
@@ -4,5 +4,7 @@ do
   envsubst < $config > /etc/postfix/${config##*/}
 done
 
+postmap /etc/postfix/NetworkTable
 /etc/postfix/post-install meta_directory=/etc/postfix create-missing
 postfix set-permissions
+postfix start-fg
